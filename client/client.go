4ba5eb24ee9dcf44def54fadf5cad819a3b758f0
Jhonathann da Silva Virtuoso <jhonathann.virtuoso@neoway.com.br>
2024-06-07 18:05:02 -0300

    fix: context

--- client/client.go
+++ client/client.go
@@ -1,58 +1,94 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type USDBRL struct {
 	Bid string `json:"bid"`
 }
 
-// Inserir os contexts
 func main() {
-	req, err := http.Get("http://localhost:8080/cotacao")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err)
-		return
-	}
-	defer req.Body.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
 
-	res, err := io.ReadAll(req.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao ler a resposta:: %v\n", err)
-		return
-	}
+	select {
+	case <-ctx.Done():
+		log.Println("tempo excedito no client.go")
+	default:
+		req, err := http.Get("http://localhost:8080/cotacao")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err)
+			return
+		}
+		defer req.Body.Close()
 
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler a resposta:: %v\n", err)
+			return
+		}
+
+		cotacao, err := TreatedResponseBody(res)
+		if err != nil {
+			return
+		}
+
+		filename := "cotacao.txt"
+		file, err := CreateFile(filename)
+		if err != nil {
+			return
+		}
+		defer file.Close()
+
+		err = WriteFile(file, filename, cotacao)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func TreatedResponseBody(res []byte) (*USDBRL, error) {
 	var cotacao USDBRL
-	err = json.Unmarshal(res, &cotacao)
+	err := json.Unmarshal(res, &cotacao)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Erro ao fazer o parse da resposta: %v\n", err)
-		return
+		return nil, err
 	}
 
 	if cotacao.Bid == "" {
 		fmt.Fprintln(os.Stderr, "A cotação está vazia, entre em contato com o suporte.")
-		return
+		return nil, err
 	}
 
-	filename := "cotacao.txt"
+	return &cotacao, nil
+}
+
+func CreateFile(filename string) (*os.File, error) {
 	file, err := os.Create(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Erro ao criar o arquivo: %v\n", err)
-		return
+		return nil, err
 	}
-	defer file.Close()
 
-	_, err = file.WriteString(fmt.Sprintf("Dólar: %s", cotacao.Bid))
+	return file, nil
+}
+
+func WriteFile(file *os.File, filename string, cotacao *USDBRL) error {
+	_, err := file.WriteString(fmt.Sprintf("Dólar: %s", cotacao.Bid))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Erro ao escrever no arquivo: %v\n", err)
-		return
+		return err
 	}
 
 	log.Printf("Escrita realizada com sucesso, verifique a cotação do Dolár no arquivo %s.", filename)
+
+	return nil
 }
--- server/server.go
+++ server/server.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type CotacaoService interface {
-	Cotacao()
+	TreatedResponseBody()
 }
 
 type Dolar struct {
@@ -19,26 +25,65 @@ type USDBRL struct {
 	Bid string `json:"bid"`
 }
 
-func BuscaUSDBRLHandler(w http.ResponseWriter, r *http.Request) {
-	cotacao := Dolar{}
+type Database struct {
+	Database *sql.DB
+}
 
-	resp, err := cotacao.Cotacao()
+func NewConnectionDB() (*Database, error) {
+	database, err := sql.Open("sqlite3", "./cotacao.db")
 	if err != nil {
+		fmt.Println("Erro ao abrir o banco de dados:", err)
+		return nil, err
+	}
+
+	return &Database{
+		Database: database,
+	}, nil
+}
+
+func BuscaUSDBRLHandler(w http.ResponseWriter, r *http.Request) {
+	cotacao := &Dolar{}
+	ctx := context.Background()
+
+	resp, err := Cotacao(ctx)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	dolar, err := cotacao.TreatedResponseBody(resp)
+	if err != nil {
+		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(resp)
-	log.Printf("A cotação do Dólar neste momento é de: $%s", resp.Bid)
+	json.NewEncoder(w).Encode(dolar)
+	log.Printf("A cotação do Dólar neste momento é de: $%s", dolar.Bid)
 }
 
-func (dr *Dolar) Cotacao() (*USDBRL, error) {
+func Cotacao(ctx context.Context) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+	defer cancel()
+
 	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-	resp, err := http.Get(url)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("request na URL %s passou do tempo limite", url)
+		default:
+			return nil, err
+		}
 	}
 	defer resp.Body.Close()
 
@@ -46,12 +91,86 @@ func (dr *Dolar) Cotacao() (*USDBRL, error) {
 	if err != nil {
 		return nil, err
 	}
-	err = json.Unmarshal(body, &dr)
+
+	return body, nil
+}
+
+func (d *Dolar) TreatedResponseBody(body []byte) (*USDBRL, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-ctx.Done():
+		return nil, fmt.Errorf("inserção da cotação no banco de dados passou do tempo limite")
+	default:
+		err := json.Unmarshal(body, &d)
+		if err != nil {
+			return nil, err
+		}
+
+		db, err := NewConnectionDB()
+		if err != nil {
+			return nil, err
+		}
+		defer db.Database.Close()
+
+		err = createTableSQL(db)
+		if err != nil {
+			return nil, err
+		}
+
+		err = insertCotacao(db, d.USDBRL.Bid)
+		if err != nil {
+			return nil, err
+		}
+
+		return &d.USDBRL, nil
+	}
+}
+
+// CREATE TABLE IF NOT EXISTS
+func createTableSQL(db *Database) error {
+	createTableStr := `CREATE TABLE IF NOT EXISTS cotacao_dolar (
+		"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,		
+		"cotacao" NUMBER,
+		"processing_date" DATE
+	)`
+	stmt, err := db.Database.Prepare(createTableStr)
 	if err != nil {
-		return nil, err
+		log.Println("Erro ao criar a tabela no SQLite: ", err)
+		return err
+	}
+	stmt.Exec()
+
+	return nil
+}
+
+func insertCotacao(db *Database, cotacao string) error {
+	insertCotacao := `INSERT INTO cotacao_dolar (cotacao, processing_date) values (?, ?)`
+	stmt, err := db.Database.Prepare(insertCotacao)
+	if err != nil {
+		log.Println("Erro ao preparar a query do SQLite: ", err)
+		return err
 	}
 
-	return &dr.USDBRL, nil
+	currentDate := generateCurrentDate()
+
+	_, err = stmt.Exec(cotacao, currentDate)
+	if err != nil {
+		log.Println("Erro ao executar a insercao: ", err)
+		return err
+	}
+
+	log.Println("Cotação inserida com sucesso!!!")
+
+	return nil
+}
+
+func generateCurrentDate() string {
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("2006-01-02 15:04:05")
+
+	return formattedTime
 }
 
 func main() {
